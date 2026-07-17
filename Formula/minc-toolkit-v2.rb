d4201c4b5ff85fc7e903a807519e9fa5cbd5c4ca
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2026-07-17 10:39:14 -0400

    minc-toolkit-v2: bump to develop-1.9.18 a5fbd6c1 (bicgl/bicpl/Register fixes)
    Pulls in the viewport signed-overflow UB fix (bicgl), the blank-slice-view
    use-after-free fix (bicpl/Register), and a libminc sync (valgrind
    uninitialized-read and macOS leak-test false-positive fixes).

--- Formula/minc-toolkit-v2.rb
+++ Formula/minc-toolkit-v2.rb
@@ -11,7 +11,7 @@ class MincToolkitV2 < Formula
   # shallow clone and checkout fails with "unable to read tree".
   url "https://github.com/BIC-MNI/minc-toolkit-v2.git",
       branch:   "develop-1.9.18",
-      revision: "9bb1ae434caf3e217a396db186f0239d82720c8e"
+      revision: "a5fbd6c188efac5e1d2595608aae63ec5e690468"
   version "1.9.18-20260305" # MINC_TOOLKIT_VERSION_FULL in CMakeLists.txt
   license "GPL-3.0-only"
 
